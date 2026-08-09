e7cd766f53bc39b32ccb16bb71650cad8851a448
rprajwal9204-droid <rprajwal9204@gmail.com>
2026-08-09 13:34:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-simport React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { supabase, supabaseConfigured } from './lib/supabase'
 import './styles.css'
